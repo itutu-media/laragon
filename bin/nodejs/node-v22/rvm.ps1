77c6272174241d4366f23374515a53b5ac4a16d2
adszt <adszt@uhb.ac.id>
2024-10-16 08:46:05 +0700

    update nodejs

---
+++ bin/nodejs/node-v22/rvm.ps1
@@ -0,0 +1,29 @@
+#!/usr/bin/env pwsh
+& "D:\Apps\Laragon\bin\nodejs\node-v22\node_modules\rvm-windows\src\tools\current\ps_rvm_prepend.ps1"
+$basedir=Split-Path $MyInvocation.MyCommand.Definition -Parent
+
+$exe=""
+if ($PSVersionTable.PSVersion -lt "6.0" -or $IsWindows) {
+  # Fix case when both the Windows and Linux builds of Node
+  # are installed in the same directory
+  $exe=".exe"
+}
+$ret=0
+if (Test-Path "$basedir/node$exe") {
+  # Support pipeline input
+  if ($MyInvocation.ExpectingInput) {
+    $input | & "$basedir/node$exe"  "$basedir/node_modules/rvm-windows/src/cli/cli.js" $args
+  } else {
+    & "$basedir/node$exe"  "$basedir/node_modules/rvm-windows/src/cli/cli.js" $args
+  }
+  $ret=$LASTEXITCODE
+} else {
+  # Support pipeline input
+  if ($MyInvocation.ExpectingInput) {
+    $input | & "node$exe"  "$basedir/node_modules/rvm-windows/src/cli/cli.js" $args
+  } else {
+    & "node$exe"  "$basedir/node_modules/rvm-windows/src/cli/cli.js" $args
+  }
+  $ret=$LASTEXITCODE
+}
+exit $ret
